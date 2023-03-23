24c6108ca93d1ce77d5665bc864b1dc4d801279a
myname <mygmail.com>
2023-03-23 07:10:52 -0700

    LOL

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,12 +6,14 @@
  *
  * Return: Always 0.
  */
-int _isupper(int c){
-
-if(c<="A" && c>="Z"){
-        return(1);
+int _isupper(int c)
+{
+if (c >= "A" && c <= "Z")
+{
+ return (1);
 }
-else{
-	return(0);
+else
+{
+return (0);
 }
 }
